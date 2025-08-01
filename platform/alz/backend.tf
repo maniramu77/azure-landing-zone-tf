50c21018b14dffaf05e73b7aba52a59f3c77259c
ManiRamu <manirr@gmail.com>
2025-08-01 16:22:28 +0530

    change the storage name

--- platform/alz/backend.tf
+++ platform/alz/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-bootstrap-rg"
-    storage_account_name = "judechenlztfsa01"
+    storage_account_name = "maniramu_storage"
     container_name       = "tfstates"
     key                  = "platform.terraform.tfstate"
   }
